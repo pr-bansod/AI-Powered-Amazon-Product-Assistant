826d98314b6c54b14048effe829f31712d80c8ad
prashant <pr.bansod23@gmail.com>
2025-10-30 18:01:51 +0900

    Fix RAG pipeline collection name and image field

--- src/api/rag/retrieval_generation.py
+++ src/api/rag/retrieval_generation.py
@@ -1,5 +1,22 @@
 import openai
+import instructor
+import numpy as np
+
+from pydantic import BaseModel, Field
 from langsmith import traceable, get_current_run_tree
+
+from qdrant_client import QdrantClient
+from qdrant_client.models import Filter, FieldCondition, MatchValue, Prefetch, FusionQuery, Document
+
+
+
+class RAGUsedContext(BaseModel):
+    id: str = Field(description="ID of the item used to answer the question.")
+    description: str = Field(description="Short description of the item used to answer the question.")
+
+class RAGGenerationResponseWithReferences(BaseModel):
+    answer: str = Field(description="Answer to the question.")
+    references: list[RAGUsedContext] = Field(description="List of items used to answer the question.")
 
 
 @traceable(
@@ -82,16 +99,19 @@ def retrieve_data(query, qdrant_client, k=5):
 
     retrieved_context_ids = []
     retrieved_context = []
+    retrieved_context_ratings = []
     similarity_scores  = []
 
     for result in results.points:
         retrieved_context_ids.append(result.payload["parent_asin"])
         retrieved_context.append(result.payload["description"])
+        retrieved_context_ratings.append(result.payload["average_rating"])
         similarity_scores.append(result.score)
 
     return {
         "retrieved_context_ids": retrieved_context_ids,
         "retrieved_context": retrieved_context,
+        "retrieved_context_ratings": retrieved_context_ratings,
         "similarity_scores": similarity_scores,
     }
 
@@ -128,8 +148,8 @@ def process_context(context):
         - B002: Bluetooth speaker
     """
     formatted_context = ""
-    for id, chunk in zip(context["retrieved_context_ids"], context["retrieved_context"]):
-        formatted_context += f"- {id}: {chunk} \n"
+    for id, chunk, rating in zip(context["retrieved_context_ids"], context["retrieved_context"],context["retrieved_context_ratings"]):
+        formatted_context += f"- {id}:, rating:{rating}, description:{chunk}\n"
 
     return formatted_context
 
@@ -164,9 +184,18 @@ You are a shopping assistant that can answer questions about the products in sto
 
 You will be given a question and a list of context.
 
-Instructions:
+Instructtions:
 - You need to answer the question based on the provided context only.
 - Never use word context and refer to it as the available products.
+- As an output you need to provide:
+
+* The answer to the question based on the provided context.
+* The list of the IDs of the chunks that were used to answer the question. Only return the ones that are used in the answer.
+* Short description (1-2 sentences) of the item based on the description provided in the context.
+
+- The short description should have the name of the item.
+- The answer to the question should contain detailed information about the product and returned with detailed specification in bullet points.
+
 
 Context:
 {preprocessed_context}
@@ -210,20 +239,23 @@ def generate_answer(prompt):
         - Temperature set to 0.5 for balanced creativity and consistency
         - Prompt is sent as a system message for stronger instruction-following
     """
-    response = openai.chat.completions.create(
-        model="gpt-4o-mini",
+    client = instructor.from_openai(openai.OpenAI())
+
+    response, raw_response = client.chat.completions.create_with_completion(
+        model="gpt-4.1-mini",
         messages=[{"role": "system", "content": prompt}],
         temperature=0.5,
+        response_model=RAGGenerationResponseWithReferences
     )
 
     current_run = get_current_run_tree()
     if current_run:
         current_run.metadata["usage_metadata"] = {
-            "input_tokens":response.usage.prompt_tokens,
-            "output_tokens": response.usage.completion_tokens,
-            "total_tokens": response.usage.total_tokens 
+            "input_tokens":raw_response.usage.prompt_tokens,
+            "output_tokens": raw_response.usage.completion_tokens,
+            "total_tokens": raw_response.usage.total_tokens 
         }
-    return response.choices[0].message.content
+    return response
     
 @traceable(
     name="rag-pipeline"
@@ -256,13 +288,47 @@ def rag_pipeline(question,qdrant_client, top_k=5):
 
     final_result = {
 
-        "answer" : answer,
-        "question":question,
+        "answer" : answer.answer,
+        "references": answer.references,
+        "question": question,
         "retrieved_context_ids": retrieved_context["retrieved_context_ids"],
         "retrieved_context": retrieved_context["retrieved_context"],
         "similarity_scores": retrieved_context["similarity_scores"]
     } 
     
     return final_result 
+
+
+def rag_pipeline_wrapper(question, top_k=5):
+
+    qdrant_client = QdrantClient(url="http://qdrant:6333")
+
+    result = rag_pipeline(question, qdrant_client, top_k)
+
+    used_context = []
+    dummy_vector = np.zeros(1536).tolist()
+
+    for item in result.get("references", []):
+        payload = qdrant_client.query_points(
+            collection_name="Amazon-items-collection-00",
+            query=dummy_vector,
+            limit=1,
+            with_payload=True,
+            query_filter=Filter(
+                must=[
+                    FieldCondition(
+                        key="parent_asin",
+                        match=MatchValue(value=item.id))
+                ]
+            )
+        ).points[0].payload
+        image_url = payload.get("images", "")
+        price = payload.get("price")
+        used_context.append({"image_url": image_url, "price": price, "description": item.description})
+
+    return {
+        "answer": result["answer"],
+        "used_context": used_context
+    }
    
 

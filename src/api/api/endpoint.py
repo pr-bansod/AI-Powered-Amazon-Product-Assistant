515c8da0b0e0ca6f704909736183938e8c6bb64e
prashant <pr.bansod23@gmail.com>
2025-10-30 18:02:26 +0900

    Update endpoint to use renamed models file

--- src/api/api/endpoint.py
+++ src/api/api/endpoint.py
@@ -1,27 +1,30 @@
+from fastapi import APIRouter, Request
 import logging
 
-from fastapi import APIRouter, Request
-from qdrant_client import QdrantClient
+from api.api.models import RAGRequest, RAGResponse, RAGUsedContext
 
-from api.api.model import RAGRequest, RAGResponse
-from api.api.rag.retrieval_generation import rag_pipeline
+from api.rag.retrieval_generation import rag_pipeline_wrapper
+
+
 
 logger = logging.getLogger(__name__)
 
 rag_router = APIRouter()
-qdrant_client = QdrantClient(url="http://qdrant:6333")
-
 
 @rag_router.post("/")
 def rag(
     request: Request,
-    payload: RAGRequest) -> RAGResponse:
-    answer = rag_pipeline(qdrant_client,payload.query)
+    payload: RAGRequest
+) -> RAGResponse:
+
+    answer = rag_pipeline_wrapper(payload.query)
+
     return RAGResponse(
         request_id=request.state.request_id,
-        answer=answer["answer"])
+        answer=answer["answer"],
+        used_context=[RAGUsedContext(**used_context) for used_context in answer["used_context"]]
+    )
 
 
 api_router = APIRouter()
-api_router.include_router(rag_router, prefix="/rag", tags=["rag"]) 
-
+api_router.include_router(rag_router, prefix="/rag", tags=["rag"])

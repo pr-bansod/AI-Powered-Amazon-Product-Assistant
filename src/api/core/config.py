676f1230a04a5f62fb492af3675f6ee59241c01d
prashant <pr.bansod23@gmail.com>
2025-11-03 14:47:16 +0900

    Update API config to include Cohere API key

--- src/api/core/config.py
+++ src/api/core/config.py
@@ -1,14 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Config(BaseSettings):
     OPENAI_API_KEY: str
     GROQ_API_KEY: str
-    LANGSMITH_API_KEY: str
-    LANGSMITH_TRACING: bool = True 
-    LANGSMITH_ENDPOINT: str = "https://api.smith.langchain.com"
-    LANGSMITH_PROJECT : str = "rag-tracing"
-
+    CO_API_KEY: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
